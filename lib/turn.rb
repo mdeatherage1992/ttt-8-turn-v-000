b1cd460f6e2de9d9563424a1f15012ee1b07aa35
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-10 18:42:54 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
+
+def position_taken?(board,index)
+  val1 = board[index]
+  if val1 == " " || val1 == "" || val1 == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board,index)
+  if position_taken?(board,index) == false && index.between?(0,8)
+    true
+  else
+    false
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def input_to_index(user_input)
+(user_input.to_i) - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
